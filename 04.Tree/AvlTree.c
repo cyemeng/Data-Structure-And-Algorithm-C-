044779dcc0a7c8880c926d804ff178c3e51c508d
Tsui yemeng <cyemeng@qq.com>
2019-04-11 22:42:59 +0800

    Commit ,add some function about avl tree.

--- 04.Tree/AvlTree.c
+++ 04.Tree/AvlTree.c
@@ -56,7 +56,24 @@ AvlTree Insert(ElementType X, AvlTree T)
 			if (X < T->Left->Element)
 			{
 				T = SingleRotateWithLeft(T);
+			} 
+			else
+			{
+				T = DooubleRotageWithLeft(T);
 			}
+		}
+		else if (X >T->Element)
+		{
+			T->Right = Insert(X, T->Right);
+			if (Height(T->Left) - Height(T->Right) == 2)
+			{
+				if (X > T->Right->Element)
+					T = SingleRotateWithRight(T);
+				else
+					T = DoubleRotateWithRight(T);
+			}
+			T->Height = Max(Height(T->Left), Height(T->Right)) + 1;
+			return T;
 		}
 	}
 }
